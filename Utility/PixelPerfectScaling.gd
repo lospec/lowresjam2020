b7cb27593c317d3830d40a79caf38ffb7c22a79d
PureAsbestos <aquajonah@gmail.com>
2020-08-01 13:16:42 -0500

    Use spaces for indentation in PixelPerfectScaling.gd

--- Utility/PixelPerfectScaling.gd
+++ Utility/PixelPerfectScaling.gd
@@ -21,30 +21,30 @@ onready var base_size = root.get_visible_rect().size
 
 
 func _ready():
-	get_tree().connect("screen_resized", self, "_on_screen_resized")
+    get_tree().connect("screen_resized", self, "_on_screen_resized")
 
-	root.set_attach_to_screen_rect(root.get_visible_rect())
-	_on_screen_resized()
+    root.set_attach_to_screen_rect(root.get_visible_rect())
+    _on_screen_resized()
 
 
 func _on_screen_resized():
-	var new_window_size = OS.window_size
+    var new_window_size = OS.window_size
 
-	var scale_w = max(int(new_window_size.x / base_size.x), 1)
-	var scale_h = max(int(new_window_size.y / base_size.y), 1)
-	var scale = min(scale_w, scale_h)
+    var scale_w = max(int(new_window_size.x / base_size.x), 1)
+    var scale_h = max(int(new_window_size.y / base_size.y), 1)
+    var scale = min(scale_w, scale_h)
 
-	var diff = new_window_size - (base_size * scale)
-	var diffhalf = (diff * 0.5).floor()
+    var diff = new_window_size - (base_size * scale)
+    var diffhalf = (diff * 0.5).floor()
 
-	root.set_attach_to_screen_rect(Rect2(diffhalf, base_size * scale))
+    root.set_attach_to_screen_rect(Rect2(diffhalf, base_size * scale))
 
-	# Black bars to prevent flickering:
-	var odd_offset = Vector2(int(new_window_size.x) % 2, int(new_window_size.y) % 2)
+    # Black bars to prevent flickering:
+    var odd_offset = Vector2(int(new_window_size.x) % 2, int(new_window_size.y) % 2)
 
-	VisualServer.black_bars_set_margins(
-		max(diffhalf.x, 0), # prevent negative values, they make the black bars go in the wrong direction.
-		max(diffhalf.y, 0),
-		max(diffhalf.x, 0) + odd_offset.x,
-		max(diffhalf.y, 0) + odd_offset.y
-	)
+    VisualServer.black_bars_set_margins(
+        max(diffhalf.x, 0), # prevent negative values, they make the black bars go in the wrong direction.
+        max(diffhalf.y, 0),
+        max(diffhalf.x, 0) + odd_offset.x,
+        max(diffhalf.y, 0) + odd_offset.y
+    )
