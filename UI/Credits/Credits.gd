2e8c78f00e1febe0d7562566018143cb045c25c4
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-01 04:28:11 +0100

    Credits improvements

--- UI/Credits/Credits.gd
+++ UI/Credits/Credits.gd
@@ -2,16 +2,25 @@ extends MarginContainer
 
 # Constants
 const SCROLL_SPEED = 5
-const MAX_SCROLL = 140
-const ON_RESET_SCROLL = 64
+const THANKS_GAP = 20
+const MAX_THANKS_POS_Y = 0
 
 # Public Variables
 var can_scroll = false
 
+# Onready Variables
+onready var main_credits_vbox = $"Main Credits"
+onready var thanks_label = $Thanks
+
+# Feels hacky but it works
+
+func _on_Credits_sort_children():
+	thanks_label.rect_position.y = main_credits_vbox.rect_size.y + THANKS_GAP
+
 func _process(delta):
-	rect_position.y -= SCROLL_SPEED * delta
-	if rect_position.y <= -MAX_SCROLL:
-		rect_position.y = ON_RESET_SCROLL
+	main_credits_vbox.rect_position.y -= SCROLL_SPEED * delta
+	if thanks_label.rect_position.y > MAX_THANKS_POS_Y:
+		thanks_label.rect_position.y -= SCROLL_SPEED * delta
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
