6da0b74789c81c6cce39c02431bb6f7603717702
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-01 14:05:39 +0100

    Placeholder color blindness button

--- UI/Settings/Settings.gd
+++ UI/Settings/Settings.gd
@@ -1,11 +1,14 @@
 extends MarginContainer
 
-func _on_Audio_pressed():
-	print_debug("Nothin' to see here.")
-
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		exit_to_main_menu()
+
+func _on_Audio_pressed():
+	print_debug("Nothin' to see here.")
+
+func _on_Color_Blindness_pressed():
+	print_debug(":Flushed:")
 
 func exit_to_main_menu():
 	if get_tree().change_scene("res://UI/Main Menu/Main Menu.tscn") != OK:
