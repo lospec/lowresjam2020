46959edd05bc34346954bf8349aa963c49af41e3
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-01 02:08:27 +0100

    Placeholder credits button

--- UI/Main Menu/Main Menu.gd
+++ UI/Main Menu/Main Menu.gd
@@ -2,3 +2,6 @@ extends MarginContainer
 
 func _on_Play_Button_pressed():
 	print_debug("There is no game, what do you expect to happen?")
+
+func _on_Credits_Button_pressed():
+	print_debug("There is no credits, what do you expect to happen?")
