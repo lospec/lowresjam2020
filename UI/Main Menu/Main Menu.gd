96ef6284be87dc6d498b53cc2cb8da35c30c6891
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-08-02 14:59:22 +0200

    Sliced forest tileset and added preset chunk

--- UI/Main Menu/Main Menu.gd
+++ UI/Main Menu/Main Menu.gd
@@ -5,9 +5,9 @@ func _on_Play_Button_pressed():
 		print_debug("An error occured while attempting to change to the world scene")
 
 func _on_Credits_Button_pressed():
-    if get_tree().change_scene("res://UI/Credits/Credits.tscn") != OK:
-        print_debug("An error occured while attempting to change to the credits scene")
+	if get_tree().change_scene("res://UI/Credits/Credits.tscn") != OK:
+		print_debug("An error occured while attempting to change to the credits scene")
 
 func _on_Settings_Button_pressed():
-    if get_tree().change_scene("res://UI/Settings/Settings.tscn") != OK:
-        print_debug("An error occured while attempting to change to the settings scene")
+	if get_tree().change_scene("res://UI/Settings/Settings.tscn") != OK:
+		print_debug("An error occured while attempting to change to the settings scene")
