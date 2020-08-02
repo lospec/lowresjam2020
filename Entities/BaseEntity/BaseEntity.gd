f008972eac385d12414bae75ec96692555107511
Arash <arashm335@gmail.com>
2020-08-02 17:15:57 +0800

    move velocity to BaseEntity;

--- Entities/BaseEntity/BaseEntity.gd
+++ Entities/BaseEntity/BaseEntity.gd
@@ -1,10 +1,23 @@
 extends KinematicBody2D
 
-export(int) var maxHealth
-var currentHealth : int
+# constants
+const SPEED = 20
 
+# Public Variables
+var velocity = Vector2()
+var velocity_leftover = Vector2()
+
+export (int) var maxHealth
+var currentHealth: int
+
+func _init():
+	self.velocity = velocity
 
 func _ready():
 	if maxHealth <= 0:
 		return
 	currentHealth = maxHealth
+
+
+func _physics_process(_delta):
+	velocity = move_and_slide(velocity)
