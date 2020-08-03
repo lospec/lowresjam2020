ae6ae1d347be180bb9a71755b250484c8e154270
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-03 23:00:04 +0100

    World improvements

--- Utility/Utility.gd
+++ Utility/Utility.gd
@@ -1,5 +1,13 @@
 extends Node
 
-static func randomRange(minim, maxim):
+func _ready():
 	randomize()
+
+static func randomRange(minim, maxim):
 	return randi() % (int(maxim - minim)) + int(minim)
+
+static func rand_element(arr):
+	return arr[randi() % arr.size()]
+
+static func rand_bool() -> bool:
+	return bool(randi() % 2)
