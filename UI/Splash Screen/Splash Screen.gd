2bda33a60055e0de7fc2d44518a4e1d6c18861e5
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-06 21:17:16 +0100

    Added some autoloaded transition utility methods and implemented them into the splash screen

--- UI/Splash Screen/Splash Screen.gd
+++ UI/Splash Screen/Splash Screen.gd
@@ -1,5 +1,11 @@
 extends Node
 
+# Constants
+const SPLASH_SCREEN_TRANSITION_DURATION = 0.3
+
+# Public Variables
+var going_to_main_menu = false
+
 # Onready Variables
 onready var animated_sprite = $AnimatedSprite
 
@@ -8,16 +14,17 @@ func _ready():
 	animated_sprite.playing = true
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		go_to_main_menu()
-
-func _unhandled_key_input(_event):
-	go_to_main_menu()
+	if event is InputEventMouseButton or event is InputEventKey:
+		if not going_to_main_menu:
+			go_to_main_menu()
 
 func _on_AnimatedSprite_animation_finished():
-	go_to_main_menu()
+	if not going_to_main_menu:
+		go_to_main_menu()
 
 func go_to_main_menu():
+	going_to_main_menu = true
+	yield(Transitions.change_scene_double_transition("res://UI/Main Menu/Main Menu.tscn",
+			Transitions.Transition_Type.SHRINKING_CIRCLE,
+			SPLASH_SCREEN_TRANSITION_DURATION), "completed")
 	PaletteSwap.enabled = true
-	if get_tree().change_scene("res://UI/Main Menu/Main Menu.tscn") != OK:
-		print_debug("An error occured while attempting to change to the main menu scene")
