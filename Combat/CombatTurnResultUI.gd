0d2b93b62ac863b6dd7e60b96d2e3dd9f4e5c525
A& <63592416+imAcedia@users.noreply.github.com>
2020-08-08 16:42:10 +0700

    Added Tween Before Showing Character Turns

--- Combat/CombatTurnResultUI.gd
+++ Combat/CombatTurnResultUI.gd
@@ -1,7 +1,11 @@
 extends MarginContainer
 
+export(float) var action_tween_delay = 1
+export(float) var action_tween_duration = 1
+
 var combat_util = preload("res://Combat/CombatUtil.gd")
 
+onready var tween: Tween = $Tween
 onready var compare_container = $CompareContainer
 onready var line_label = $LineLabel
 onready var win_container = $WinResultContainer
@@ -10,7 +14,7 @@ onready var enemy_action_label = $CompareContainer/EnemyTurnContainer/ActionLabe
 onready var win_actor_label = $WinResultContainer/ActorLabel
 onready var win_action_label = $WinResultContainer/ActionLabel
 
-func show_turn_compare(playerAction, enemyAction, duration = 0):
+func show_turn_compare(playerAction, enemyAction, duration = 1.5):
 	player_action_label.text = combat_util.GetActionName(playerAction)
 	player_action_label.modulate = combat_util.GetActionColor(playerAction)
 	
@@ -21,7 +25,21 @@ func show_turn_compare(playerAction, enemyAction, duration = 0):
 	compare_container.visible = true
 	
 	if duration > 0:
+		player_action_label.modulate.a = 0
+		enemy_action_label.modulate.a = 0
+
+		tween.interpolate_property(enemy_action_label, "modulate:a",
+				0, 1, action_tween_duration, Tween.TRANS_QUAD, Tween.EASE_OUT,
+				action_tween_delay)
+		
+		tween.interpolate_property(player_action_label, "modulate:a",
+				0, 1, action_tween_duration, Tween.TRANS_QUAD, Tween.EASE_OUT,
+				action_tween_delay)
+		
+		tween.start()
+		yield(tween, "tween_all_completed")
 		yield(get_tree().create_timer(duration), "timeout")
+		
 		line_label.visible = false
 		compare_container.visible = false
 
