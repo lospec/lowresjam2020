dc92c16a88cc652a69c81d767c9355f14683dc20
Arash <arashm335@gmail.com>
2020-08-12 00:48:22 +0800

    add _on_start method to actions;

--- AI/Actions/AI_Action_Wander.gd
+++ AI/Actions/AI_Action_Wander.gd
@@ -5,15 +5,15 @@ export (float) var update_time = 1
 
 var _timer: float
 var _move: Vector2
-var _init = false
+
+
+func _on_start():
+	_timer = update_time
+
 
 func perform(_stateMachine, _delta, _interrupt):
-	if not _init:
-		_init = true
-		_timer = update_time
-		
 	if _timer >= update_time:
 		_timer = 0
-		_move = Vector2(rand_range(-1,1), rand_range(-1,1)) * move_speed
+		_move = Vector2(rand_range(-1, 1), rand_range(-1, 1)) * move_speed
 	_stateMachine.entity.velocity = _move
 	_timer += _delta
--- AI/Actions/AI_State_Action.gd
+++ AI/Actions/AI_State_Action.gd
@@ -1,4 +1,11 @@
 extends Resource
 
-func perform(_stateMachine, _delta, _interrupt):
+var _is_init = false
+
+func _on_start():
 	pass
+
+func perform(_stateMachine, _delta, _interrupt):
+	if not _is_init:
+		_is_init = true
+		_on_start()
