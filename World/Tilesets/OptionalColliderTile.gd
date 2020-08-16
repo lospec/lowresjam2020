b454ca4a38c5b876a9426dc38e542233ee964ca2
Arash <arashm335@gmail.com>
2020-08-16 23:46:43 +0800

    fix tile autotiling;

--- World/Tilesets/OptionalColliderTile.gd
+++ World/Tilesets/OptionalColliderTile.gd
@@ -1,17 +1,27 @@
 tool
 extends TileSet
 
-func bind_tiles(drawn_id, neighbor_id, x,y):
+const RULES = [
+	[0, 15],
+	[2, 16],
+	[12, 17],
+	[41, 35],
+	[0, 35],
+	[0, 41],
+]
+func bind_tiles(drawn_id, neighbor_id, x, y):
 	if drawn_id == x and neighbor_id == y:
 		return true
 	if drawn_id == y and neighbor_id == x:
 		return true
+	return false
 
 func _is_tile_bound(drawn_id, neighbor_id):
-	bind_tiles(drawn_id, neighbor_id, 0, 15)
-	bind_tiles(drawn_id, neighbor_id, 2, 16)
-	bind_tiles(drawn_id, neighbor_id, 12, 17)
-	bind_tiles(drawn_id, neighbor_id, 41, 35)
-	bind_tiles(drawn_id, neighbor_id, 0, 35)
-	bind_tiles(drawn_id, neighbor_id, 0, 41)
-	return drawn_id == neighbor_id
+	if drawn_id == neighbor_id:
+		return true
+
+	for rule in RULES:
+		if bind_tiles(drawn_id, neighbor_id, rule[0], rule[1]):
+			return true
+			
+	return false
