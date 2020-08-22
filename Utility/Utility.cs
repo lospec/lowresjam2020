eb9e02045c2103849bb22321b6bf1f313485fcb7
Arash <arashm335@gmail.com>
2020-08-22 12:02:16 +0800

    add Utility extension methods;

---
+++ Utility/Utility.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace HeroesGuild.Utility
+{
+    public static class Utility
+    {
+        static Utility()
+        {
+            GD.Randomize();
+        }
+
+        public static T RandomElement<T>(this IEnumerable<T> list)
+        {
+            return RandomElement(list.ToArray());
+        }
+        public static T RandomElement<T>(this IList<T> list)
+        {
+            var idx = (int) (GD.Randi() % list.Count);
+            return list[idx];
+        }
+    }
+}
