edf5d3da328f956bd76658e064cb54ee9ccceef1
Arash <arashm335@gmail.com>
2020-08-22 09:06:19 +0800

    update PaletteSwap;

--- Utility/PaletteSwap.cs
+++ Utility/PaletteSwap.cs
@@ -2,43 +2,46 @@ using Godot;
 
 namespace HeroesGuild.Utility
 {
-	public class PaletteSwap : CanvasLayer
-	{
-		[Export] private bool _enabled;
-		[Export] private Texture _palette;
+    [Tool]
+    public class PaletteSwap : CanvasLayer
+    {
+        private bool _enabled;
+        private Texture _palette;
 
-		private TextureRect _paletteSwapTextureRect;
+        private TextureRect _paletteSwapTextureRect;
 
-		public bool Enabled
-		{
-			set
-			{
-				_enabled = value;
-				if (!HasNode(_paletteSwapTextureRect.Name))
-				{
-					return;
-				}
+        [Export] public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                _enabled = value;
+                if (!HasNode("TextureRect"))
+                {
+                    return;
+                }
 
-				((ShaderMaterial) _paletteSwapTextureRect.Material).SetShaderParam("enabled", _enabled);
-			}
-		}
-		public Texture Palette
-		{
-			set
-			{
-				_palette = value;
-				if (!HasNode(_paletteSwapTextureRect.Name))
-				{
-					return;
-				}
+                ((ShaderMaterial) _paletteSwapTextureRect.Material).SetShaderParam("enabled", _enabled);
+            }
+        }
+        [Export] public Texture Palette
+        {
+            get => _palette;
+            set
+            {
+                _palette = value;
+                if (!HasNode("TextureRect") || value == null)
+                {
+                    return;
+                }
 
-				((ShaderMaterial) _paletteSwapTextureRect.Material).SetShaderParam("palette_tex", _palette);
-			}
-		}
+                ((ShaderMaterial) _paletteSwapTextureRect.Material).SetShaderParam("palette_tex", _palette);
+            }
+        }
 
-		public override void _Ready()
-		{
-			_paletteSwapTextureRect = GetNode<TextureRect>("TextureRect");
-		}
-	}
+        public override void _Ready()
+        {
+            _paletteSwapTextureRect = GetNode<TextureRect>("TextureRect");
+        }
+    }
 }
