cddc18a95eb62dba7782f771e217210b691a5760
Arash <arashm335@gmail.com>
2020-08-23 07:30:48 +0800

    Update SaveData to use List instead of Array;

--- Utility/SaveData.cs
+++ Utility/SaveData.cs
@@ -10,12 +10,12 @@ namespace HeroesGuild.Utility
         private const string DEFAULT_ARMOR = "";
         private const int DEFAULT_HEALTH = 20;
         private static readonly Vector2 DefaultWorldPosition = new Vector2(2578, 1517);
-        private static readonly string[] DefaultInventory = {"Stick", "Hotdog"};
+        private static readonly List<string> DefaultInventory = new List<string> {"Stick", "Hotdog"};
 
         public Vector2 WorldPosition { get; set; } = DefaultWorldPosition;
         public string CharacterName { get; set; } = "Jason";
         public int Coins { get; set; } = DEFAULT_COINS;
-        public string[] Inventory { get; set; } = DefaultInventory;
+        public List<string> Inventory { get; set; } = DefaultInventory;
         public string EquippedWeapon { get; set; } = DEFAULT_WEAPON;
         public string EquippedArmor { get; set; } = DEFAULT_ARMOR;
         public int MaxHealth { get; set; } = DEFAULT_HEALTH;
