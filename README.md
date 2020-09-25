fd934b91825222c80473ac7b01e4872396beeb06
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-25 17:08:19 +0100

    Removed everything again
    I hope this is the last time

--- README.md
+++
@@ -1,1 +0,0 @@
-# lowresjam2020
--- ai/Conditions/AI_State_Condition.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace HeroesGuild.AI.Conditions
-{
-    public abstract class AI_State_Condition : Resource
-    {
-        public abstract bool Evaluate(StateMachine stateMachine);
-    }
-}
